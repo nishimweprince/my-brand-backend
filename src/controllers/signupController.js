e6077d675f07dccc5087deaa890a0af2a26ddcb6
Nishimwe Prince <princeelysee@gmail.com>
2023-03-06 19:55:34 +0200

    Completed demo-ready progress
        - Prevented creating duplicate blogs.
        - Fixed ERR_HTTP_HEADERS_SENT by preventing setting headers multiple times.
        - Added role based categorization on users.

--- src/controllers/signupController.js
+++ src/controllers/signupController.js
@@ -1,36 +1,70 @@
-import User from '../models/user.js';
-import bcrypt from 'bcrypt';
-import errorFunc from '../utils/errors.js';
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+import errorFunc from "../utils/errors.js";
 
 const singupController = async (req, res) => {
+  const { username, email, password } = req.body;
 
-    const { username, email, password, role } = req.body;
+  try {
+    // HASH PASSWORD
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    try {
+    // MAKE THE FIRST USER AN ADMIN
+    const users = await User.find();
 
-        // HASH PASSWORD
-        const hashedPassword = await bcrypt.hash(password, 10);
+    // CHECK IF A USER ALREADY EXISTS
+    const userExists = await User.findOne({ email });
 
+    // CHECK IF THERE ARE USERS
+    if (users.length === 0) {
+        
+    if (userExists) {
+
+    return res.status(400).json({
+        message: "User already exists",
+    });
+
+    } else {
         // CREATE A NEW USER
-        const user = await User.create({username, email, password: hashedPassword, role});
-
-        // SEND RESPONSE
-        res.status(201).json({
-            message: "User created successfully",
-            data: user.email
+        const user = await User.create({
+            username,
+            email,
+            password: hashedPassword,
+            role: "admin",
         });
-
-        console.log(user)
-
-
+    }
     }
 
-    catch (error) {
-        const message = error.message;
-        const status = 500;
-        errorFunc(res, message, status)
+    else {
+
+        // CHECK IF USER EXISTS
+        if (userExists) {
+            return res.status(400).json({
+                message: "User already exists"
+            });
+        }
+
+        else {
+        // CREATE A NEW USER
+        const user = await User.create({
+            username,
+            email,
+            password: hashedPassword,
+            role: "user",
+        });
+    }
     }
 
+    // SEND RESPONSE
+    res.status(201).json({
+      message: "User created successfully",
+    });
+
+  } catch (error) {
+    const message = error.message;
+    const status = 500;
+    errorFunc(res, message, status);
+  }
 };
 
 export default singupController;
